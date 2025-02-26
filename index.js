a11af0e8dccdf2a9c8b722b2977fd33e93643ede
kurashin234 <sabu18802@gmail.com>
2025-02-26 14:21:34 +0900

    bcryptからbcryptjsへ変更

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const express = require('express');
 const mysql = require('mysql2');
 const app = express();
